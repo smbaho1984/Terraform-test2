db78023da59e141f36133d2b8e20c101413c5e12
smbaho1984 <stephanembaho@gmail.com>
2023-02-08 11:48:49 -0500

    fouth

--- provider.tf
+++ provider.tf
@@ -1,5 +1,12 @@
- provider "aws" {
-  region = var.aws_region
- # profile = var.aws_profile
+
+provider "aws" {
+  region  = "us-east-2"
 }
+
+
+
+ #provider "aws" {
+ # region = var.aws_region
+ # profile = var.aws_profile
+#}
 
