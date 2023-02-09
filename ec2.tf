5d7526f05d965567acdd8a8a53269de425356ad9
smbaho1984 <stephanembaho@gmail.com>
2023-02-09 08:51:43 -0500

    last

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "app" {
   ami           = var.ami_id[1]
-  instance_type = var.instance_type[1]
+  instance_type = var.instance_type[0]
 
   tags = {
     Name = var.ec2_name_tag[0]
